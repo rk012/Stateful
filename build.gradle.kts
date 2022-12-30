59474a82d00d5469199ae80511eff54877d2e06c
Rishi <24rkumar@student.dasd.org>
2022-12-30 14:06:22 -0500

    Added maven publish plugin for deployment on jitpack
    Moved gradle build logic to main build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-
-group = "io.github.rk012"
-version = "0.1-SNAPSHOT"
-
-allprojects {
+buildscript {
     repositories {
         mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
+
+allprojects {
+    group = "io.github.rk012"
+    version = "0.1-SNAPSHOT"
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
+
+    val testImplementation by configurations
+
+    dependencies {
+        testImplementation(kotlin("test"))
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+        }
     }
 }
